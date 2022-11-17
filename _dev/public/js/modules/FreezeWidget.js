a422c9d6bed6dfed2c000eaeb43a7cadca13da5d
Alfredo Arena <alfredo.arena@ctmobi.it>
2022-11-17 12:47:08 +0100

    perf(freezewidget.js): remove jQuery from FreezeWidget module

--- _dev/public/js/modules/FreezeWidget.js
+++ _dev/public/js/modules/FreezeWidget.js
@@ -1,20 +1,26 @@
-import $ from 'jquery';
 
 class FreezeWidget {
   constructor(widgetContainer) {
     this.widgetContainer = widgetContainer;
-    this.ratingItems = $(this.widgetContainer + '.js-rmp-rating-item');
-    this.ratingTextContainer = $(this.widgetContainer + '.js-rmp-hover-text');
-    this.submitBtn = $(this.widgetContainer + '.js-submit-rating-btn');
+    this.ratingItems = document.querySelectorAll(this.widgetContainer + '.js-rmp-rating-item');
+    this.ratingTextContainer = document.querySelector(this.widgetContainer + '.js-rmp-hover-text');
+    this.submitBtn = document.querySelector(this.widgetContainer + '.js-submit-rating-btn');
     this.events();
   }
 
   events() {
-    this.ratingItems.off();
-    this.submitBtn.off();
-    this.ratingItems.css('cursor', 'default');
-    this.submitBtn.removeClass('rmp-rating-widget__submit-btn--visible');
-    $(this.ratingTextContainer).text('');
+    this.ratingItems.forEach((item) => {
+      // Remove all event listeners from element.
+      item.replaceWith(item.cloneNode(true));
+    })
+    this.ratingItems = document.querySelectorAll(this.widgetContainer + '.js-rmp-rating-item');
+    this.submitBtn.replaceWith(this.submitBtn.cloneNode(true));
+    this.submitBtn = document.querySelector(this.widgetContainer + '.js-submit-rating-btn');
+    this.ratingItems.forEach((item) => {
+      item.style.cursor = 'default';
+    })
+    this.submitBtn.classList.remove('rmp-rating-widget__submit-btn--visible');
+    this.ratingTextContainer.textContent = '';
   }
 }
 
